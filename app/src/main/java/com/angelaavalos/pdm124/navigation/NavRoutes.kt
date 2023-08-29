097a5da27be5296e77fecd96eff23aec149b6bb0
Angela <angelaavalos2410@gmail.com>
2023-08-28 18:29:38 -0600

    Se agregan cambios

--- app/src/main/java/com/angelaavalos/pdm124/navigation/NavRoutes.kt
+++ app/src/main/java/com/angelaavalos/pdm124/navigation/NavRoutes.kt
@@ -1,4 +1,4 @@
-package com.angelaavalos.pdm124
+package com.angelaavalos.pdm124.navigation
 
 /**
  * Lista de rutas a las cuales se puede navegar dentro de la aplicación
@@ -8,4 +8,6 @@ sealed class NavRoutes (val route: String){
     object firstpartial : NavRoutes("firstpartial")
     object secondpartial : NavRoutes("secondpartial")
     object thirdpartial : NavRoutes("thirdpartial")
+    object padelscore : NavRoutes("padelscore")
+    object evenorodd : NavRoutes("evenorodd")
 }
